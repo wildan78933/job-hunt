aa4f7c5922a3c26601837f5b1965ca9710350e83
wildan <wildan78933@gmail.com>
2024-03-06 11:46:57 +0700

    update

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+import { optionType } from "@/types";
+
+export const LOCATION_OPTIONS: optionType[] = [
+  {
+    id: "Indonesia",
+    label: "Indonesia",
+  },
+
+  {
+    id: "Malaysia",
+    label: "Malaysia",
+  },
+
+  {
+    id: "Singapura",
+    label: "Singapura",
+  },
+];
